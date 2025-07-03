90c8d0b7e589897361cbcc3c0e899e4b14d11591
AmitBiswas1402 <amit.142biswas@gmail.com>
2025-07-04 00:36:47 +0530

    Refactor NavRoutes component; add Button component and update dependencies

--- app/(dashboard)/_components/NavRoutes.tsx
+++ app/(dashboard)/_components/NavRoutes.tsx
@@ -1,16 +1,44 @@
-"use client"
-import { UserButton } from "@clerk/nextjs"
-import { usePathname } from "next/navigation"
+"use client";
+import { Button } from "@/components/ui/button";
+import { UserButton } from "@clerk/nextjs";
+import { LogOut } from "lucide-react";
+import Link from "next/link";
+import { usePathname } from "next/navigation";
 
 const NavRoutes = () => {
-    const pathname = usePathname();
+  const pathname = usePathname();
 
-    const isAdmin = pathname.startsWith("/admin");
+  const isAdmin = pathname.startsWith("/admin");
+  const isEmp = pathname.startsWith("/jobs");
 
   return (
     <div className="flex gap-x-2 ml-auto">
-        <UserButton afterSignOutUrl="/" />
+      {isAdmin || isEmp ? (
+        <Link href={"/"}>
+          <Button
+            variant={"outline"}
+            size={"sm"}
+            className="border-purple-700/20"
+          >
+            <LogOut />
+            Exit
+          </Button>
+        </Link>
+      ) : (
+        <>
+          <Link href={"/admin/jobs"}>
+            <Button
+              variant={"outline"}
+              size={"sm"}
+              className="border-purple-700/20"
+            >
+              Admin Mode
+            </Button>
+          </Link>
+        </>
+      )}
+      <UserButton afterSignOutUrl="/" />
     </div>
-  )
-}
-export default NavRoutes
+  );
+};
+export default NavRoutes;
